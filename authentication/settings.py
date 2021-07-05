489aa9002fcb2e340c6ae0e305c67aa44a55cd7c
JKP-2001 <jaypandey6957@gmail.com>
2021-07-05 12:08:47 +0530

    Initial Commit

--- authentication/settings.py
+++ authentication/settings.py
@@ -144,7 +144,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
