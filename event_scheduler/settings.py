d084ab439ed71c68331e68878c53ca605aa92fa5
danshCode <soumadipdas18@yahoo.com>
2022-01-04 17:00:39 +0530

    code cleaned

--- event_scheduler/settings.py
+++ event_scheduler/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'event_scheduler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
