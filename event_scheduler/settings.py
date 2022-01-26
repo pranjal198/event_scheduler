c0f57f413131661af2ba36d9d728656af7849aa6
JKP-2001 <84286157+JKP-2001@users.noreply.github.com>
2022-01-27 01:06:02 +0530

    Update setting.py

--- event_scheduler/settings.py
+++ event_scheduler/settings.py
@@ -151,11 +151,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/event-scheduler/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/event-scheduler/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
